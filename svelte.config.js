abba2971ed5f18bc1856f6bfc9cd1be09b309b93
Caleb Miller <caleb@mathmammoth.com>
2023-06-24 20:33:22 -0600

    b

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
