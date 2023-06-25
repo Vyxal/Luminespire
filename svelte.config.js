c16116c44d23d4e87986863291c3b2ea3db1d7a3
Lyxal <36217120+Lyxal@users.noreply.github.com>
2023-06-25 23:31:34 +1000

    Make svelte actually link all the pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,15 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH,
+		}
+	}
 };
 
 export default config;
