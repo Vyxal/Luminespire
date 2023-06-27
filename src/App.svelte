c1f0eaaa218d878316ac6dbfd7abe618f6ae39cf
ysthakur <45539777+ysthakur@users.noreply.github.com>
2023-06-26 20:06:46 -0400

    Spaces > tabs. Fight me.

--- src/App.svelte
+++ src/App.svelte
@@ -1,190 +1,190 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import Sortable from 'sortablejs';
+  import { onMount } from 'svelte';
+  import Sortable from 'sortablejs';
 
-	interface Line {
-		code: number[][];
-		input: string;
-		id: number;
-	}
+  interface Line {
+    code: number[][];
+    input: string;
+    id: number;
+  }
 
-	let text = '';
-	$: textLines = text.split('\n');
+  let text = '';
+  $: textLines = text.split('\n');
 
-	let commentChar = '#';
+  let commentChar = '#';
 
-	let lines: Line[] = [];
+  let lines: Line[] = [];
 
-	let selectedLine: null | number = null;
+  let selectedLine: null | number = null;
 
-	function addLine() {
-		lines.push({
-			code: [],
-			input: '',
-			id: Math.round(Math.random() * 1000) + new Date().getTime()
-		});
-		lines = lines;
-	}
+  function addLine() {
+    lines.push({
+      code: [],
+      input: '',
+      id: Math.round(Math.random() * 1000) + new Date().getTime()
+    });
+    lines = lines;
+  }
 
-	let linesEl;
-	let explanationEl;
-	onMount(() => {
-		Sortable.create(linesEl, {
-			group: {
-				name: 'lines',
-				put: true
-			},
-			animation: 200,
-			chosenClass: 'cursor-grabbing',
-			onEnd: (ev) => {
-				if (lines.length <= 1) return;
-				[lines[ev.oldIndex], lines[ev.newIndex]] = [lines[ev.newIndex], lines[ev.oldIndex]];
-				if (selectedLine === ev.oldIndex) {
-					selectedLine = ev.newIndex;
-				} else if (selectedLine === ev.newIndex) {
-					selectedLine = ev.oldIndex;
-				}
-				lines = lines;
-			}
-		});
-	});
+  let linesEl;
+  let explanationEl;
+  onMount(() => {
+    Sortable.create(linesEl, {
+      group: {
+        name: 'lines',
+        put: true
+      },
+      animation: 200,
+      chosenClass: 'cursor-grabbing',
+      onEnd: (ev) => {
+        if (lines.length <= 1) return;
+        [lines[ev.oldIndex], lines[ev.newIndex]] = [lines[ev.newIndex], lines[ev.oldIndex]];
+        if (selectedLine === ev.oldIndex) {
+          selectedLine = ev.newIndex;
+        } else if (selectedLine === ev.newIndex) {
+          selectedLine = ev.oldIndex;
+        }
+        lines = lines;
+      }
+    });
+  });
 
-	function select(row: number, col: number) {
-		resizeExplanationTextarea();
-		if (selectedLine === null) return;
-		if (!lines[selectedLine].code[row]) {
-			lines[selectedLine].code[row] = [];
-		}
-		const index = lines[selectedLine].code[row].indexOf(col);
-		if (index >= 0) {
-			lines[selectedLine].code[row].splice(index, 1);
-		} else {
-			lines[selectedLine].code[row].push(col);
-			lines[selectedLine].code[row].sort((a, b) => a - b);
-		}
-		lines = lines;
-	}
+  function select(row: number, col: number) {
+    resizeExplanationTextarea();
+    if (selectedLine === null) return;
+    if (!lines[selectedLine].code[row]) {
+      lines[selectedLine].code[row] = [];
+    }
+    const index = lines[selectedLine].code[row].indexOf(col);
+    if (index >= 0) {
+      lines[selectedLine].code[row].splice(index, 1);
+    } else {
+      lines[selectedLine].code[row].push(col);
+      lines[selectedLine].code[row].sort((a, b) => a - b);
+    }
+    lines = lines;
+  }
 
-	$: maxLen = Math.max(...textLines.map((r) => r.length));
-	$: explanation = [
-		text,
-		...lines.flatMap((line) => {
-			const rows = textLines
-				.map(
-					(row, r) =>
-						line.code[r]?.length &&
-						[...row].map((x, i) => (line.code[r]?.includes(i) ? x : ' ')).join('')
-				)
-				.filter((x) => x);
-			if (rows.length == 0) {
-				return [];
-			} else {
-				return [rows[0].padEnd(maxLen, ' ') + `  ${commentChar} ` + line.input, ...rows.slice(1)];
-			}
-		})
-	].join('\n');
+  $: maxLen = Math.max(...textLines.map((r) => r.length));
+  $: explanation = [
+    text,
+    ...lines.flatMap((line) => {
+      const rows = textLines
+        .map(
+          (row, r) =>
+            line.code[r]?.length &&
+            [...row].map((x, i) => (line.code[r]?.includes(i) ? x : ' ')).join('')
+        )
+        .filter((x) => x);
+      if (rows.length == 0) {
+        return [];
+      } else {
+        return [rows[0].padEnd(maxLen, ' ') + `  ${commentChar} ` + line.input, ...rows.slice(1)];
+      }
+    })
+  ].join('\n');
 
-	function resizeTextarea(e) {
-		e.target.style.height = 0;
-		e.target.style.height = e.target.scrollHeight + 10 + 'px';
-	}
+  function resizeTextarea(e) {
+    e.target.style.height = 0;
+    e.target.style.height = e.target.scrollHeight + 10 + 'px';
+  }
 
-	function resizeExplanationTextarea() {
-		explanationEl.style.height = 0;
-		explanationEl.style.height = explanationEl.scrollHeight + 10 + 'px';
-	}
+  function resizeExplanationTextarea() {
+    explanationEl.style.height = 0;
+    explanationEl.style.height = explanationEl.scrollHeight + 10 + 'px';
+  }
 </script>
 
 <div class="p-5">
-	<h1 class="text-4xl font-bold text-center">Luminespire - The Explanation Assistant</h1>
-	<div class="font-bold text-xl">Program</div>
-	<textarea
-		on:input={resizeTextarea}
-		id="program"
-		class="resize-none min-h-[50px] border border-gray-400 p-2 h-24 font-mono w-full mt-2 outline-none focus:ring rounded"
-		bind:value={text}
-	/>
-	<div class="flex-col gap-3">
-		{#each textLines as row, r}
-			<div class="flex flex-wrap gap-3 my-4">
-				{#each row as char, c}
-					<div
-						class="text-lg cursor-pointer py-1 px-3"
-						class:bg-gray-200={selectedLine === null || !lines[selectedLine].code[r]?.includes(c)}
-						class:bg-yellow-400={selectedLine !== null && lines[selectedLine].code[r]?.includes(c)}
-						on:click={() => select(r, c)}
-						on:keypress={() => select(r, c)}
-						role="checkbox"
-						aria-checked={lines[selectedLine]?.code[r]?.includes(c)}
-						tabindex={c}
-					>
-						{char}
-					</div>
-				{/each}
-			</div>
-		{/each}
-	</div>
+  <h1 class="text-4xl font-bold text-center">Luminespire - The Explanation Assistant</h1>
+  <div class="font-bold text-xl">Program</div>
+  <textarea
+    on:input={resizeTextarea}
+    id="program"
+    class="resize-none min-h-[50px] border border-gray-400 p-2 h-24 font-mono w-full mt-2 outline-none focus:ring rounded"
+    bind:value={text}
+  />
+  <div class="flex-col gap-3">
+    {#each textLines as row, r}
+      <div class="flex flex-wrap gap-3 my-4">
+        {#each row as char, c}
+          <div
+            class="text-lg cursor-pointer py-1 px-3"
+            class:bg-gray-200={selectedLine === null || !lines[selectedLine].code[r]?.includes(c)}
+            class:bg-yellow-400={selectedLine !== null && lines[selectedLine].code[r]?.includes(c)}
+            on:click={() => select(r, c)}
+            on:keypress={() => select(r, c)}
+            role="checkbox"
+            aria-checked={lines[selectedLine]?.code[r]?.includes(c)}
+            tabindex={c}
+          >
+            {char}
+          </div>
+        {/each}
+      </div>
+    {/each}
+  </div>
 
-	<div class="font-bold text-xl">Lines</div>
-	<ul bind:this={linesEl}>
-		{#each lines as line, idx (line.id)}
-			<li class="flex grid-cols-4 items-center gap-3 cursor-grab">
-				<div
-					class="w-5 h-5 cursor-pointer"
-					class:bg-gray-300={idx !== selectedLine}
-					class:bg-blue-500={idx === selectedLine}
-					on:click={() => (selectedLine = idx)}
-					on:keypress={() => (selectedLine = idx)}
-					role="radio"
-					aria-checked={idx === selectedLine}
-					tabindex={idx}
-				/>
-				<div class="w-1/8">Line {idx + 1}</div>
-				<div class="w-1/4">
-					<code
-						>{line.code.flatMap((row, r) => row?.map((c) => textLines[r][c]) ?? []).join('')}
-					</code>
-				</div>
-				<div class="w-1/4">
-					<textarea
-						bind:value={line.input}
-						class="border border-gray-400 p-2 outline-none focus:ring rounded w-full"
-					/>
-				</div>
-				<div>
-					<button on:click={() => (lines.splice(idx, 1), (lines = lines))} class="btn"
-						>Remove</button
-					>
-					<button on:click={() => (line.code = [])} class="btn">Clear</button>
-				</div>
-			</li>
-		{/each}
-	</ul>
+  <div class="font-bold text-xl">Lines</div>
+  <ul bind:this={linesEl}>
+    {#each lines as line, idx (line.id)}
+      <li class="flex grid-cols-4 items-center gap-3 cursor-grab">
+        <div
+          class="w-5 h-5 cursor-pointer"
+          class:bg-gray-300={idx !== selectedLine}
+          class:bg-blue-500={idx === selectedLine}
+          on:click={() => (selectedLine = idx)}
+          on:keypress={() => (selectedLine = idx)}
+          role="radio"
+          aria-checked={idx === selectedLine}
+          tabindex={idx}
+        />
+        <div class="w-1/8">Line {idx + 1}</div>
+        <div class="w-1/4">
+          <code
+            >{line.code.flatMap((row, r) => row?.map((c) => textLines[r][c]) ?? []).join('')}
+          </code>
+        </div>
+        <div class="w-1/4">
+          <textarea
+            bind:value={line.input}
+            class="border border-gray-400 p-2 outline-none focus:ring rounded w-full"
+          />
+        </div>
+        <div>
+          <button on:click={() => (lines.splice(idx, 1), (lines = lines))} class="btn"
+            >Remove</button
+          >
+          <button on:click={() => (line.code = [])} class="btn">Clear</button>
+        </div>
+      </li>
+    {/each}
+  </ul>
 
-	<br />
+  <br />
 
-	<button class="btn" on:click={addLine}>Add Line</button>
+  <button class="btn" on:click={addLine}>Add Line</button>
 
-	<br />
-	<br />
+  <br />
+  <br />
 
-	<div class="font-bold text-xl">Comment character</div>
-	<input bind:value={commentChar} class="border" />
+  <div class="font-bold text-xl">Comment character</div>
+  <input bind:value={commentChar} class="border" />
 
-	<br />
-	<br />
+  <br />
+  <br />
 
-	<div class="grid grid-cols-1">
-		<div class="font-bold text-xl">Explanation</div>
-		<!-- make this text area expand upon input -->
-		<textarea
-			bind:this={explanationEl}
-			readonly
-			value={explanation}
-			class="border border-gray-400 rounded p-2 outline-none font-mono mt-2"
-			cols="50"
-			rows="10"
-			style="resize: none; height: 20px;"
-		/>
-	</div>
+  <div class="grid grid-cols-1">
+    <div class="font-bold text-xl">Explanation</div>
+    <!-- make this text area expand upon input -->
+    <textarea
+      bind:this={explanationEl}
+      readonly
+      value={explanation}
+      class="border border-gray-400 rounded p-2 outline-none font-mono mt-2"
+      cols="50"
+      rows="10"
+      style="resize: none; height: 20px;"
+    />
+  </div>
 </div>
