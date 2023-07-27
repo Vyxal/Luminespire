480e70d1957aa67debca630a5123bce91fe5d896
ysthakur <45539777+ysthakur@users.noreply.github.com>
2023-07-27 14:40:59 -0400

    Move sidebar stuff back into Sidebar

--- src/Sidebar.svelte
+++ src/Sidebar.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
   import { fly, slide } from 'svelte/transition';
+  import TextArea from './TextArea.svelte';
 
   export let sidebarShown: boolean = false;
-  /** class/classes for the stuff inside the sidebar */
-  export let contentClass: string = '';
+  export let selectedTheme: string = localStorage.theme || 'os';
+  export let commentChar = '#';
+  export let importFromText: (text: string) => void;
 
   let sidebar: HTMLDivElement;
+  let importValue = '';
 
   const slideDuration = 300;
   const transitionDelay = 200;
@@ -13,6 +16,29 @@
   function toggleSidebar() {
     sidebarShown = !sidebarShown;
   }
+
+  function shouldUseDark() {
+    return 'theme' in localStorage
+      ? localStorage.theme !== 'light'
+      : !window.matchMedia('(prefers-color-scheme: light)').matches;
+  }
+  let darkTheme = shouldUseDark();
+  $: {
+    if (selectedTheme === 'os') {
+      localStorage.removeItem('theme');
+    } else if (selectedTheme) {
+      localStorage.theme = selectedTheme;
+    }
+    darkTheme = shouldUseDark();
+  }
+  $: if (darkTheme) {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+  }
+  window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', e => {
+    darkTheme = shouldUseDark();
+  });
 
   $: icon = sidebarShown ? 'fa-xmark text-red-700' : 'fa-bars text-black dark:text-white';
 </script>
@@ -31,15 +57,45 @@
     <br />
     <br />
     <div
-      class={contentClass}
+      class="text-black dark:text-white"
       in:fly={{ delay: transitionDelay, duration: slideDuration }}
       out:fly={{ duration: slideDuration }}>
-      <slot />
+      <div>
+        <div class="flex items-stretch">
+          <strong class="text-2xl">Options</strong>
+        </div>
+        <br />
+        <div class="flex items-stretch">
+          <p class="pr-5 text-xl font-bold sm:pr-10">Comment character</p>
+          <TextArea bind:value={commentChar} class="w-12 border sm:w-16" rows={1} />
+        </div>
+      </div>
+      <br />
+      <div>
+        <p class="text-xl font-bold">Import Options</p>
+        <br />
+        <TextArea class="mt-5" bind:value={importValue} />
+        <br />
+        <button
+          class="btn mt-3 sm:mt-5"
+          on:click={() => importFromText(importValue)}
+          on:keypress={() => importFromText(importValue)}>Import from Explanation</button>
+      </div>
+      <br />
+      <div>
+        <label for="theme" class="text-xl font-bold">Theme:</label>
+        <select bind:value={selectedTheme} class="bg-gray-100 dark:bg-[#333333]">
+          <option value="os" selected={!('theme' in localStorage)}>Sync with OS</option>
+          <option value="dark" selected={localStorage.theme === 'dark'}>Dark</option>
+          <option value="light" selected={localStorage.theme === 'light'}>Light</option>
+        </select>
+      </div>
     </div>
   </div>
 
+  <!-- Overlay to shadow everything else. When clicked, sidebar closes -->
   <div
-    class="overlay visible absolute left-0 top-0 z-50 h-screen w-screen"
+    class="overlay visible fixed left-0 top-0 z-50 h-screen w-screen"
     on:click={toggleSidebar}
     on:keypress={toggleSidebar}
     transition:fly={{ duration: slideDuration }} />
