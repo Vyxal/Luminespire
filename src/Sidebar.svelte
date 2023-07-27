7691cbf6169b7a1d93626ec410d3f3a8894be4de
ysthakur <45539777+ysthakur@users.noreply.github.com>
2023-07-27 14:31:18 -0400

    Sidebar overlay to close sidebar

--- src/Sidebar.svelte
+++ src/Sidebar.svelte
@@ -23,32 +23,26 @@
   on:keypress={toggleSidebar} />
 
 {#if sidebarShown}
-  <!-- This outer div covers the entire viewport and has a fixed position. This
-    allows the overlay to have a fixed position while still being positioned
-    relative to the sidebar (since both are children of this outer div)-->
-  <div class="invisible fixed inset-0 z-50 h-screen w-screen p-0">
+  <div
+    bind:this={sidebar}
+    class="visible fixed left-0 top-0 z-[100] h-screen w-screen bg-gray-50 p-5 dark:bg-zinc-900 sm:w-1/4"
+    in:slide={{ duration: slideDuration, axis: 'x' }}
+    out:slide={{ delay: transitionDelay, duration: slideDuration, axis: 'x' }}>
+    <br />
+    <br />
     <div
-      bind:this={sidebar}
-      class="visible relative left-0 top-0 h-screen w-screen bg-gray-50 p-5 dark:bg-zinc-900 sm:w-1/4"
-      in:slide={{ duration: slideDuration, axis: 'x' }}
-      out:slide={{ delay: transitionDelay, duration: slideDuration, axis: 'x' }}>
-      <br />
-      <br />
-      <div
-        class={contentClass}
-        in:fly={{ delay: transitionDelay, duration: slideDuration }}
-        out:fly={{ duration: slideDuration }}>
-        <slot />
-      </div>
-    </div>
-
-    <div
-      class="overlay visible relative left-0 top-0 h-screen w-screen bg-gray-200 dark:bg-black"
-      on:click={toggleSidebar}
-      on:keypress={toggleSidebar}>
-      foo
+      class={contentClass}
+      in:fly={{ delay: transitionDelay, duration: slideDuration }}
+      out:fly={{ duration: slideDuration }}>
+      <slot />
     </div>
   </div>
+
+  <div
+    class="overlay visible absolute left-0 top-0 z-50 h-screen w-screen"
+    on:click={toggleSidebar}
+    on:keypress={toggleSidebar}
+    transition:fly={{ duration: slideDuration }} />
 {/if}
 
 <style>
@@ -56,9 +50,13 @@
     position: fixed;
     top: 0;
     left: 0;
-    z-index: 100; /* Make sure it's at the top */
     width: 30px;
     height: 30px;
     margin: 20px;
+    z-index: 1000;
+  }
+
+  .overlay {
+    box-shadow: inset 0px 0px 0 2000px rgba(0, 0, 0, 0.3);
   }
 </style>
