b62b9f3eccce7dfc2b0cf1f73314d2be3d033844
lyxal <36217120+Lyxal@users.noreply.github.com>
2024-09-21 00:15:09 +1000

    v0!

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   darkMode: 'class',
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#5432c3',
+        'primary-dark': '#4a2ca8',
+      },
+    },
   },
   plugins: []
 };
